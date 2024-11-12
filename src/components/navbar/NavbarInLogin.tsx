2bad7b3e0d5e51f559adef8944e5394b6d9f1f2f
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-13 08:01:48 +0900

    refactor(narbar): responsive UI for Login button

--- src/components/navbar/NavbarInLogin.tsx
+++ src/components/navbar/NavbarInLogin.tsx
@@ -17,7 +17,10 @@ export default function NavbarInLogin() {
           <span className='text-[15px] font-semibold mr-1 mt-[2px] lg:mt-[10px]'>{session.user?.name} 님</span>
         </div>
       ) : (
-        <Link href='/login' className='font-bold bg-blue-500 p-2 text-white rounded-3xl hover:bg-blue-600 duration-200'>
+        <Link
+          href='/login'
+          className='font-bold bg-blue-500 mt-[12px] p-2 lg:mt-0 text-white rounded-3xl hover:bg-blue-600 duration-200'
+        >
           LOGIN
         </Link>
       )}
