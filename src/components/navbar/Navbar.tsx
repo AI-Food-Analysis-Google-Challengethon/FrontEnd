abaef24784a0638b786df9744eca029a5436fd07
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-13 05:00:58 +0900

    refactor(navbar): put back button to the left on Navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -4,15 +4,16 @@ import BackButton from './BackButton';
 export default function Navbar() {
   return (
     <nav className='fixed bg-white top-0 left-0 z-50 w-full h-[80px] border-b-2 flex justify-center items-center shadow-sm'>
-      <div></div>
+      <div className='absolute left-3 top-5'>
+        <BackButton />
+      </div>
       <Link href='/' className='text-blue-400 text-3xl  font-bold  p-2 rounded-3xl hover:bg-blue-100 hover:scale-'>
         천리안
       </Link>
-      <div className='absolute top-5 right-1 flex justify-center items-center gap-4 mr-[40px]'>
+      <div className='absolute top-5 right-3 flex justify-center items-center gap-4'>
         <Link href='/login' className='text-blue-500 text-lg font-semibold hover:bg-blue-100 p-2 rounded-3xl'>
           Login
         </Link>
-        <BackButton />
       </div>
     </nav>
   );
