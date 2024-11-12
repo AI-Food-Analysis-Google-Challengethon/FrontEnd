348c13c698cf8105120fa47ee92937f8772afab7
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-13 05:13:14 +0900

    update: navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
       <Link href='/' className='text-blue-400 text-3xl  font-bold  p-2 rounded-3xl hover:bg-blue-100 hover:scale-'>
         천리안
       </Link>
-      <div className='absolute top-5 right-1 flex justify-center items-center gap-4 mr-[15px] lg:mr-[40px]'>
+      <div className='absolute right-3 top-5 flex justify-center items-center gap-4 mr-[15px] lg:mr-[40px]'>
         <Link href='/login' className='text-blue-500 text-lg font-semibold hover:bg-blue-100 p-2 rounded-3xl '>
           Login
         </Link>
