8d78fdeda093ac77bcd115eca3968a509425e767
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-16 02:13:03 +0900

    fix

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,11 @@
 import GoogleLoginButton from '@/components/auth/GoogleLoginButton';
-import SessionGuard from '@/components/auth/SessionGuard';
 
 export default function LoginPage() {
   return (
-    <SessionGuard>
-      <div className='w-full min-h-screen flex flex-col items-center mt-[200px]'>
-        <main>
-          <GoogleLoginButton />
-        </main>
-      </div>
-    </SessionGuard>
+    <div className='w-full min-h-screen flex flex-col items-center mt-[200px]'>
+      <main>
+        <GoogleLoginButton />
+      </main>
+    </div>
   );
 }
