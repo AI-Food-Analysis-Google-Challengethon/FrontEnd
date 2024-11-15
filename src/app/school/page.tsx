313fffafeec19006d2fb4b809d87bd72101f3707
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-15 23:16:03 +0900

    fix: fix Props in school page

--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -1,12 +1,12 @@
 import NutritionForm from './_components/NutritionForm';
 
-interface Props {
+export default function SchoolPage({
+  searchParams: { date },
+}: {
   searchParams: {
     date: string;
   };
-}
-
-export default function SchoolPage({ searchParams: { date } }: Props) {
+}) {
   return (
     <div className='max-w-4xl mx-auto p-6'>
       return <NutritionForm initialDate={date} />
