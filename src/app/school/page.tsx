34509bf886c382d1de5c12bb9b0a19c5ef2f46b8
eunwoo-levi <eunwoo1341@gmail.com>
2024-11-15 23:35:01 +0900

    fix: fix props

--- src/app/school/page.tsx
+++ src/app/school/page.tsx
@@ -1,15 +1,15 @@
 import NutritionForm from './_components/NutritionForm';
 
-interface SearchParams {
-  searchParams: {
-    date?: string; // 옵셔널로 변경
-  };
-}
+export default function SchoolPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
+  const date = searchParams.date as string;
 
-export default function SchoolPage({ searchParams }: SearchParams) {
   return (
     <div className='max-w-4xl mx-auto p-6'>
-      <NutritionForm initialDate={searchParams.date || ''} />
+      <NutritionForm initialDate={date} />
     </div>
   );
 }
